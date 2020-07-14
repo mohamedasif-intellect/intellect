9b4abe94bf23d367129dc947b8f54f6c6d830043
mohamed asif <r.mohamedasifh@gmail.com>
2020-07-14 08:06:54 +0400

    commited today

---
+++ BadFanTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BadFanTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		Fan2 f= new b();
+		f.test();
+		assertEquals("the speed of Fan is medium","the speed of Fan is medium");
+	}
+
+}
